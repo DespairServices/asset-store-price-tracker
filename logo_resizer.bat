d9830efdbf2895535ffa47a6de060d3de2a481c8
RampantDespair <roguewars@outlook.com>
2024-05-03 15:06:46 -0400

    Added new icon sizes

--- logo_resizer.bat
+++ logo_resizer.bat
@@ -3,6 +3,8 @@
 powershell -Command "magick convert private/logo.png -resize 16x  public/icons/icon_16.png"
 powershell -Command "magick convert private/logo.png -resize 32x  public/icons/icon_32.png"
 powershell -Command "magick convert private/logo.png -resize 48x  public/icons/icon_48.png"
+powershell -Command "magick convert private/logo.png -resize 64x  public/icons/icon_64.png"
+powershell -Command "magick convert private/logo.png -resize 96x  public/icons/icon_96.png"
 powershell -Command "magick convert private/logo.png -resize 128x public/icons/icon_128.png"
 
 pause
